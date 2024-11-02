333188fc50d972fd3238b95cc5b63798aff11b0c
wingie125 <arbab.vns123@gmail.com>
2024-11-02 11:43:28 +0530

    Complete db structure and dashboard Structure

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,24 @@
+import { DashboardPage } from "@/components/dashboard-page"
+import { db } from "@/db"
+import { currentUser } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const Page = async () => {
+  const auth = await currentUser()
+
+  if (!auth) {
+    redirect("/sign-in")
+  }
+
+  const user = await db.user.findUnique({
+    where: { externalId: auth.id },
+  })
+
+  if (!user) {
+    return redirect("/welcome")
+  }
+
+  return <DashboardPage title="Dashboard">Dashboard</DashboardPage>
+}
+
+export default Page
